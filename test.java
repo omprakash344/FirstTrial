d8179afa57ce57819c4528fc3508f5b8eae28852
omprakash344 <omprakashpr2022@gmail.com>
2023-10-24 18:48:38 +0530

    om upadate the code

--- test.java
+++ test.java
@@ -2,22 +2,10 @@
 import java.util.Scanner;
 public class abc {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b= sc.nextInt();
-        int z= sc.nextInt();
-        int c=a+b;
-        int d=a-b;
-        int e=a*b;
-        int f=a/b;
-        System.out.println("Addition of 2 number:" +   c);
-        System.out.println("Substation of 2 number:" +   d);
-        System.out.println("Multiplication of 2 number:" +  e);
-        System.out.println("division of 2 number:" +   f);
-        System.out.println(z);
-        System.out.println("Hii good morning");
-        System.out.println("hii Good Morning");
-        System.out.print("Hello who are u");
+      
+
+
+
 
 
     }
